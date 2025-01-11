c5447005ca63c5e91c4f5b4b8d5088aa27373867
gauravdubey19 <dubeygaurav520@gmail.com>
2025-01-11 12:00:04 +0530

    upd - styles and session check..

--- context/SessionProvider.jsx
+++ context/SessionProvider.jsx
@@ -1,32 +1,36 @@
 "use client";
-// getSession,
-import { SessionProvider } from "next-auth/react";
-// import { useEffect, useState } from "react";
+
+import { getSession, SessionProvider } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const AuthProvider = ({ children, session }) => {
-  // const [currentSession, setCurrentSession] = useState(session);
+  const [currentSession, setCurrentSession] = useState(session);
 
-  // useEffect(() => {
-  //   let intervalId;
+  useEffect(() => {
+    let intervalId;
 
-  //   const checkSession = async () => {
-  //     if (!currentSession?.user?._id) {
-  //       const updatedSession = await getSession();
-  //       if (updatedSession?.user?._id) {
-  //         setCurrentSession(updatedSession);
-  //         clearInterval(intervalId); // stoping the interval once the session is updated
-  //       }
-  //     }
-  //   };
+    const checkSession = async () => {
+      if (!currentSession?.user?._id) {
+        const updatedSession = await getSession();
+        if (updatedSession?.user?._id) {
+          setCurrentSession(updatedSession);
+          clearInterval(intervalId); // stoping the interval once the session is updated
+        }
+      }
+    };
 
-  //   intervalId = setInterval(() => {
-  //     checkSession();
-  //   }, 1000);
+    intervalId = setInterval(() => {
+      checkSession();
+    }, 2000);
 
-  //   return () => clearInterval(intervalId);
-  //   // if (!currentSession?.user?._id) checkSession();
-  // }, [currentSession, session]);
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+    return () => clearInterval(intervalId);
+    // if (!currentSession?.user?._id) checkSession();
+  }, [currentSession, session]);
+  return (
+    <SessionProvider session={currentSession?.user?._id && currentSession}>
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default AuthProvider;
